0472161914e3a7df30b35463f2c466859d053892
Raul Suarez <raul.suarez@eldeseazo.com>
2014-08-22 11:41:27 +0200

    A several features

---
+++ src/main/java/com/mycompany/flight/language/LocaleBean.java
@@ -0,0 +1,30 @@
+package com.mycompany.flight.language;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author raulsuarez
+ */
+@Named(value = "localeBean")
+@SessionScoped
+public class LocaleBean implements Serializable {
+
+    private HashMap<String, Locale> locales = null;
+
+    public LocaleBean() {
+        locales = new HashMap<>(2);
+        locales.put("EN", new Locale("en", "EN"));
+        locales.put("ES", new Locale("es", "ES"));
+    }
+
+    public void chooseLocaleFromLink(String local) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getViewRoot().setLocale((Locale) locales.get(local));
+    }
+}
