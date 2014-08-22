1bfbd0dfec6b2f2a06c498c364d89507dea892d5
Raul Suarez <raul.suarez@eldeseazo.com>
2014-08-22 12:32:35 +0200

    A multilanguage

--- src/main/java/com/mycompany/flight/language/LocaleBean.java
+++ src/main/java/com/mycompany/flight/language/LocaleBean.java
@@ -17,12 +17,19 @@ public class LocaleBean implements Serializable {
 
     private HashMap<String, Locale> locales = null;
 
+    /**
+     * Default constructor
+     */
     public LocaleBean() {
         locales = new HashMap<>(2);
         locales.put("EN", new Locale("en", "EN"));
         locales.put("ES", new Locale("es", "ES"));
     }
 
+    /**
+     * Switch location to new one
+     * @param local new location to apply
+     */
     public void chooseLocaleFromLink(String local) {
         FacesContext context = FacesContext.getCurrentInstance();
         context.getViewRoot().setLocale((Locale) locales.get(local));
