00e4574141386f9bc8c80462e94882b04b2ee195
raulsuarezdabo <raul.suarez.dabo@gmail.com>
2015-07-21 19:28:48 +0200

    Fixing redirect on check if the user is making a book

--- src/main/java/com/mycompany/flight/security/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/mycompany/flight/security/handler/MyAuthenticationSuccessHandler.java
@@ -6,8 +6,10 @@
  */
 package com.mycompany.flight.security.handler;
 
+import com.mycompany.flight.utils.SessionConstantsName;
 import com.raulsuarezdabo.flight.entity.UserEntity;
 import java.io.IOException;
+import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,13 +17,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication a) throws IOException, ServletException {
-        try {
+        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey(SessionConstantsName.BOOKINGSEARCH) == true) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/booking-process/seats?faces-redirect=true");
+        } else {
             UserEntity user = (UserEntity) a.getPrincipal();
-            response.sendRedirect(request.getContextPath() + "/index.xhtml");
-        } catch (Exception e) {
             response.sendRedirect(request.getContextPath() + "/index.xhtml");
         }
     }
