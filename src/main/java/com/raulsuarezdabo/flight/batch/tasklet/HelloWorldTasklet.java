c78d1239d6bdf0d0ce354497453876bbf430f4b0
raulsuarezdabo <raul.suarez.dabo@gmail.com>
2015-09-27 19:32:47 +0200

    Refactored structure to embed spring batch to web container

--- src/main/java/com/raulsuarezdabo/flight/batch/tasklet/HelloWorldTasklet.java
+++ src/main/java/com/raulsuarezdabo/flight/batch/tasklet/HelloWorldTasklet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.raulsuarezdabo.flight.batch.tasket;
+package com.raulsuarezdabo.flight.batch.tasklet;
 
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
