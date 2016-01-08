b62165fc2eb71e763ae7ffc1c742aff120a9e766
raulsuarezdabo <raul.suarez.dabo@gmail.com>
2016-01-08 19:37:35 +0100

    [Refactor] AuthentificationHandler package under raulsuarezdabo

--- src/main/java/com/raulsuarezdabo/flight/security/handler/MyAuthenticationFailureHandler.java
+++ src/main/java/com/raulsuarezdabo/flight/security/handler/MyAuthenticationFailureHandler.java
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-package com.mycompany.flight.security.handler;
+package com.raulsuarezdabo.flight.security.handler;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
